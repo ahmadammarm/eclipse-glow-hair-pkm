d74533c9926ee929eedc6ea5e630fb8ef9fd8398
Ahmad Ammar <ammarmusyaffa11@gmail.com>
2023-08-28 21:18:29 +0700

    update

--- script.js
+++ script.js
@@ -89,3 +89,16 @@ document.getElementById("aboutButton").addEventListener("click", function () {
   // Mengarahkan pengguna ke bagian "about" saat tombol diklik
   window.location.href = "#about";
 });
+
+const header = document.querySelector("[data-header]");
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY >= 80) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+});
